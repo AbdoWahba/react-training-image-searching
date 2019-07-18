912b3deb8546080f1a400a676d0039f38a3206ac
AbdoWahba <abdo.tw20154@gmail.com>
2019-07-18 23:42:07 +0200

    prevent defaults in form submittal and refactor uncontrolled event handler int controlled one

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,16 +2,24 @@ import React from "react";
 import "./SearchBar.css";
 
 class SearchBar extends React.Component {
+  state = { term: "" };
   render() {
     return (
       <div className="ui segment pic-input">
-        <form className="ui form">
+        <form
+          className="ui form"
+          onSubmit={event => {
+            event.preventDefault();
+            console.log(this.state.term);
+          }}
+        >
           <div className="field">
             <label>Image search</label>
             <input
               type="text"
+              value={this.state.term}
               onChange={event => {
-                console.log(event.target.value);
+                this.setState({ term: event.target.value });
               }}
             />
           </div>
