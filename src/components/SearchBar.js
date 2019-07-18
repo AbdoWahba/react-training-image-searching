e19918dc97d569b20fc8d916943639abaa8c0463
AbdoWahba <abdo.tw20154@gmail.com>
2019-07-18 17:08:38 +0200

    refactoring event handler function to arrow function

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,17 +2,18 @@ import React from "react";
 import "./SearchBar.css";
 
 class SearchBar extends React.Component {
-  handleUserInput(event) {
-    console.log(event.target.value);
-  }
-
   render() {
     return (
       <div className="ui segment pic-input">
         <form className="ui form">
           <div className="field">
             <label>Image search</label>
-            <input type="text" onChange={this.handleUserInput} />
+            <input
+              type="text"
+              onChange={event => {
+                console.log(event.target.value);
+              }}
+            />
           </div>
         </form>
       </div>
