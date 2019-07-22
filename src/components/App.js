2741e2cf25dd08cf73e99fee5dc374aac3f31b3c
AbdoWahba <abdo.tw20154@gmail.com>
2019-07-22 17:10:33 +0200

    using axios and update state of App component with array of images

--- src/components/App.js
+++ src/components/App.js
@@ -3,18 +3,24 @@ import Axios from "axios";
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
+  state = { image: [] };
   render() {
     return (
       <div className="ui container">
         <SearchBar
-          onSearchBarSubmit={term => {
-            Axios.get("https://api.unsplash.com/search/photos/", {
-              params: { query: term },
-              headers: {
-                Authorization:
-                  "Client-ID f8947d24a0a585d8ddb0389a18b13819423f1fb43368804d4fc133ddf1558611"
+          onSearchBarSubmit={async term => {
+            const response = await Axios.get(
+              "https://api.unsplash.com/search/photos/",
+              {
+                params: { query: term },
+                headers: {
+                  Authorization:
+                    "Client-ID f8947d24a0a585d8ddb0389a18b13819423f1fb43368804d4fc133ddf1558611"
+                }
               }
-            });
+            );
+            this.setState({ images: response.data.results });
+            console.log(response.data.results);
           }}
         />
       </div>
