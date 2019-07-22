94d8bbc82cd6a477b2d8319a51e5196fba1227a1
AbdoWahba <abdo.tw20154@gmail.com>
2019-07-22 08:15:50 +0200

    add axios.get function to fetch data

--- src/components/App.js
+++ src/components/App.js
@@ -8,7 +8,13 @@ class App extends React.Component {
       <div className="ui container">
         <SearchBar
           onSearchBarSubmit={term => {
-            console.log(term);
+            Axios.get("https://api.unsplash.com/search/photos/", {
+              params: { query: term },
+              headers: {
+                Authorization:
+                  "Client-ID f8947d24a0a585d8ddb0389a18b13819423f1fb43368804d4fc133ddf1558611"
+              }
+            });
           }}
         />
       </div>
