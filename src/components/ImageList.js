1b0d69627107e70d6c93c52589f605c584a64a45
AbdoWahba <abdo.tw20154@gmail.com>
2019-07-23 02:40:13 +0200

    refactor map function

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -3,8 +3,8 @@ import "./ImageList.css";
 
 class ImageList extends React.Component {
   render() {
-    const images = this.props.images.map(img => {
-      return <img src={img.urls.regular} key={img.id} />;
+    const images = this.props.images.map(({ description, id, urls }) => {
+      return <img src={urls.regular} key={id} alt={description} />;
     });
     console.log(this.props.images);
     return <div>{images}</div>;
