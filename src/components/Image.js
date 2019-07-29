8710a58d39916e3c19a772abceaa9c45a1c0d6fd
AbdoWahba <abdo.tw20154@gmail.com>
2019-07-30 01:37:04 +0200

    fixing grid issue using dom ref

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,14 +1,27 @@
 import React from "react";
 
 class Image extends React.Component {
-  render() {
+  constructor(props) {
+    super(props);
+    this.imageRef = React.createRef();
+    this.state = { hieght: 150 };
+  }
+  componentDidMount() {
+    console.log(this.imageRef.current.clientHeight);
+    this.setState({ hieght: this.imageRef.current.clientHeight });
+  }
+  render = () => {
     return (
       <img
+        ref={this.imageRef}
         src={this.props.image.urls.regular}
         alt={this.props.image.description}
+        style={{
+          gridRowEnd: `span ${Math.ceil((this.state.hieght + 10) / 10)}`
+        }}
       />
     );
-  }
+  };
 }
 
 export default Image;
